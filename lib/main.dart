7df317172fe143ad1d06bd3bd31df5d439295f2e
Matthieu <matthieunadeau96@gmail.com>
2021-12-01 14:04:55 -0500

    Add FlutterConfig to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_config/flutter_config.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await FlutterConfig.loadEnvVariables();
   runApp(MyApp());
 }
 
