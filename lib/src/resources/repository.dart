31e2411ce258197f259cfa8e6a615ee746e771a2
Matthieu <matthieunadeau96@gmail.com>
2021-12-01 14:05:16 -0500

    Create repository

---
+++ lib/src/resources/repository.dart
@@ -0,0 +1,8 @@
+import 'package:bloc_crypto/src/models/item_model.dart';
+import 'package:bloc_crypto/src/resources/crypto_api_provider.dart';
+
+class Repository {
+  final cryptoApiProvider = CryptoApiProvider();
+
+  Future<ItemModel> fetchAllCoins() => cryptoApiProvider.fetchCryptoList();
+}
