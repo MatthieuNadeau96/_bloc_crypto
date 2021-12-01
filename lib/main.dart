f04b0cc4a5efa606aeace0705640b0eb5dc4beb3
Matthieu <matthieunadeau96@gmail.com>
2021-12-01 11:39:24 -0500

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('hello'),
+        child: Text('hi'),
       ),
     );
   }
