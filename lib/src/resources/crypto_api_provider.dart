d0d4e6c7a0224c36b9a73165ee0706fe76687192
Matthieu <matthieunadeau96@gmail.com>
2021-12-01 14:05:08 -0500

    Create Crypto_api_provider

---
+++ lib/src/resources/crypto_api_provider.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:bloc_crypto/src/models/item_model.dart';
+import 'package:http/http.dart' show Client;
+import 'dart:convert';
+import 'package:flutter_config/flutter_config.dart';
+
+class CryptoApiProvider {
+  Client client = Client();
+  final _apiKey = FlutterConfig.get('API_KEY');
+  Uri uri = Uri.parse(
+      'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest/key/');
+
+  Future<ItemModel> fetchCryptoList() async {
+    print('entered');
+    final response = await client.get(
+      uri,
+      headers: {
+        'X-CMC_PRO_API_KEY': _apiKey,
+      },
+    );
+    print(response.body.toString());
+    if (response.statusCode == 200) {
+      return ItemModel.fromJson(
+        json.decode(response.body),
+      );
+    } else {
+      throw Exception('Failed to load');
+    }
+  }
+}
